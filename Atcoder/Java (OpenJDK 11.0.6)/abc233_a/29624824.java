62ca34a3fea8140e3a2b9f46aeb393478219288e
Imran Hossain <50188662+ImranHossain00@users.noreply.github.com>
2022-11-02 03:51:33 +0600

    Committing files

---
+++ Atcoder/Java (OpenJDK 11.0.6)/abc233_a/29624824.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String... args) {
+        Scanner input = new Scanner(System.in);
+        long n = input.nextLong();
+        if (n < 0 && n%10 != 0) n = n/10L-1;
+        else n = n/10L;
+        System.out.println(n);
+    }
+
+
+    public static int[] sort(int[] arr) {
+        int mid, len, i, j, k;
+        len = arr.length;
+        mid = len/2;
+        if (len == 1) {
+            return arr;
+        }
+
+        int[] a1 = sort(Arrays.copyOfRange(arr, 0, mid));
+        int[] a2 = sort(Arrays.copyOfRange(arr, mid, len));
+
+        i = j = k = 0;
+        while (i < a1.length && j < a2.length) {
+            if (a1[i] > a2[j]) arr[k] = a2[j++];
+            else arr[k] = a1[i++];
+            k++;
+        }
+        if (i < a1.length) while (k < len) arr[k++] = a1[i++];
+        else while (k < len) arr[k++] = a2[j++];
+        return arr;
+
+    }
+}
