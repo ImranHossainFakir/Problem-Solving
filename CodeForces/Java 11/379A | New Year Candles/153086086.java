5714f729682f6d904a281852c91d68e7b3dbf23f
Imran Hossain <50188662+ImranHossain00@users.noreply.github.com>
2022-11-02 03:57:00 +0600

    Committing files

---
+++ CodeForces/Java 11/379A | New Year Candles/153086086.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String... args) {
+        Scanner input = new Scanner(System.in);
+ 
+        int n, b, ans;
+        n = input.nextInt();
+        b = input.nextInt();
+        ans = n;
+ 
+        while (n > b-1) {
+            ans += n/b;
+            n = n/b+n%b;
+        }
+        System.out.println(ans);
+    }
+ 
+ 
+ 
+ 
+    static class FastScanner {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer("");
+ 
+        public String next() {
+            while (!st.hasMoreElements())
+                try {
+                    st=new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+ 
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+ 
+        public int[] readArray(int n) {
+            int[] a=new int[n];
+            for (int i=0; i<n; i++) {
+                a[i]=nextInt();
+            }
+            return a;
+        }
+ 
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+ 
+        public int gcd(int a, int b) {
+            if (a % b == 0) return b;
+            return gcd(b,a%b);
+        }
+ 
+        public int lcm(int a, int b, int g) {
+            return a/g*b;
+        }
+        public int[] sort(int[] arr) {
+            int mid, len, i, j, k;
+            len = arr.length;
+            mid = len/2;
+            if (len == 1) {
+                return arr;
+            }
+ 
+            int[] a1 = sort(Arrays.copyOfRange(arr, 0, mid));
+            int[] a2 = sort(Arrays.copyOfRange(arr, mid, len));
+ 
+            i = j = k = 0;
+            while (i < a1.length && j < a2.length) {
+                if (a1[i] > a2[j]) arr[k] = a2[j++];
+                else arr[k] = a1[i++];
+                k++;
+            }
+            if (i < a1.length) while (k < len) arr[k++] = a1[i++];
+            else while (k < len) arr[k++] = a2[j++];
+            return arr;
+ 
+        }
+ 
+    }
+}
