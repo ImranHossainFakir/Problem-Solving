38ec4c7e73f56b3fe7d0ad65efe67bfd1cd6813f
Imran Hossain <50188662+ImranHossain00@users.noreply.github.com>
2022-11-02 03:59:07 +0600

    Committing files

---
+++ CodeForces/Java 11/630A | Again Twenty Five!/146138438.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String... args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(25);
+    }
+}
