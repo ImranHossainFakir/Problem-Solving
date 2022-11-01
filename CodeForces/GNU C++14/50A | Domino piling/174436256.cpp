eb2e778b6b5e8712d0117ebbb2f2816335a0e7ea
Imran Hossain <50188662+ImranHossain00@users.noreply.github.com>
2022-11-02 03:54:09 +0600

    Committing files

---
+++ CodeForces/GNU C++14/50A | Domino piling/174436256.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+     
+using namespace std;
+ 
+//#define int long long
+#define endl "\n"
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define pb push_back // add in last position
+#define pop pop_back() // remove last value
+#define bk back() // return the last value
+#define mem(a, b) memset(a, b ,sizeof(a))
+#define len(a) sizeof(a) / sizeof(int)
+#define all(x) (x).begin(), (x).end()
+ 
+int32_t main() {
+    IOS;
+ 
+    int n, m; cin >> n >> m;
+ 
+    int grid = n * m;
+ 
+    cout << grid / 2 << endl;
+    return 0;
+}
