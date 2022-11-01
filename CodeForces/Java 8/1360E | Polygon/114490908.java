a44ce127eaa853f651cc3aa8e3a8275442bfa6bb
Imran Hossain <50188662+ImranHossain00@users.noreply.github.com>
2022-11-02 04:01:53 +0600

    Committing files

---
+++ CodeForces/Java 8/1360E | Polygon/114490908.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+ 
+public class BWOWFactor {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+ 
+        while (t-- > 0) {
+            int n, i, j;
+            boolean flag = true;
+            n = in.nextInt();
+            String[] numbers = new String[n];
+            for (i = 0; i < n; i++) numbers[i] = in.next();
+            inner : for (i = 0; i < n-1; i++) {
+                for (j = 0; j < n - 1; j++) {
+                    if (numbers[i].charAt(j) == '1') {
+                        if (numbers[i + 1].charAt(j) == '1' || numbers[i].charAt(j + 1) == '1') continue;
+                        flag = false;
+                        i = n;
+                        continue inner;
+                    }
+                }
+            }
+ 
+            System.out.println(flag ? "YES" : "NO");
+        }
+    }
+}
