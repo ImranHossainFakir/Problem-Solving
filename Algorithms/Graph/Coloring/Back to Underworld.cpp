5df6f31742dfac6efc1f8f36c9371d28c952198f
Imran Hossain <50188662+ImranHossain00@users.noreply.github.com>
2023-04-30 02:33:47 +0600

    Create Back to Underworld.cpp

---
+++ Algorithms/Graph/Coloring/Back to Underworld.cpp
@@ -0,0 +1,85 @@
+/*
+* problem link -> https://lightoj.com/problem/back-to-underworld
+*/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//#define int long long
+#define endl "\n"
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define pb push_back
+#define mem(a, b) memset(a, b ,sizeof(a))
+#define loop(b, e) for (int i = b; i < e; i++) 
+#define lc (n * 2)
+#define rc ((n * 2) + 1)
+#define mid ((e+b) / 2)
+
+const int N = 2e5 + 9;
+const int M = 2e4 + 9;
+
+vector< int > v[N];
+int vis[N];
+int cnt[2];
+int color[N];
+int flag[M];
+
+void dfs(int node, int col) {
+	
+	if (vis[node] == 1) return;
+	vis[node] = 1;
+
+	cnt[col]++;
+	
+	for (auto u : v[node]) {
+		
+		dfs(u, col ^ 1);
+	}
+}
+
+void solve(int cs) {
+	
+	int n; cin >> n;
+	mem(vis, 0);
+	mem(flag, 0);
+	
+
+	for (int i = 0; i < n; i++) {
+		int a, b; cin >> a >> b;
+		v[a].pb(b);
+		v[b].pb(a);
+		flag[a] = 1;
+		flag[b] = 1; 
+	}
+
+	cnt[0] = cnt[1] = 0;
+	int ans = 0;
+
+	for (int i = 1; i <= 20001; i++) {
+		if (flag[i] == 0) continue;
+		dfs(i, 0);
+		int mx = max(cnt[0], cnt[1]);
+		ans += mx;
+		cnt[0] = cnt[1] = 0;
+	}
+
+
+	cout << "Case " << cs << ": " << ans << endl;
+
+	for (auto& u : v) u.clear();
+	
+	return;
+}
+
+
+int32_t main() {
+	IOS;
+
+	int t; cin >> t;
+
+	for (int i = 1; i <= t; i++) {
+
+		solve(i);
+	}
+	return 0;
+}
