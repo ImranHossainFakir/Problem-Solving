efc123c4f8c74764105d8a7b7cd44b30539d15af
Imran Hossain <50188662+ImranHossain00@users.noreply.github.com>
2023-11-22 23:49:45 +0600

    Create Find_Prime_Factors.cpp

---
+++ Algorithms/Number Theory/Templates/Find_Prime_Factors.cpp
@@ -0,0 +1,39 @@
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+// #define int long long
+#define endl "\n"
+#define Faster                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define mem(a, b) memset(a, b, sizeof(a))
+#define len(a) sizeof(a) / sizeof(int)
+#define what_is(x) cerr << #x << " is " << x << endl;
+#define all(x) (x).begin(), (x).end()
+
+
+void count_prime_divisors(map < int, int >& divisors)
+{
+    int factor = 2;
+    while (factor * factor <= v) 
+    {
+        while (v % factor == 0)
+        {
+            divisors[factor]++;
+            v /= factor;
+        }
+        factor++;
+    }
+    if (v > 1) divisors[v]++;
+}
+
+int32_t main()
+{
+    Faster;
+    map < int, int > divisors;
+    count_prime_divisors(divisors)
+    return 0;
+}
