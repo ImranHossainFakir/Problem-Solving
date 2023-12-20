23540fb3f2620820a06c8b5b2b63f0f295b2d202
ImranHossain00 <imran188566@gmail.com>
2023-12-20 20:25:25 +0600

    updated

---
+++ Regular/B_Preparing_for_the_Contest.cpp
@@ -0,0 +1,37 @@
+
+
+/*
+
+Problem link: https://codeforces.com/contest/1914/problem/B
+Time Complexity: 
+
+*/
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+//#define int long long
+#define endl "\n" 
+#define Faster ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define mem(a, b) memset(a, b ,sizeof(a))
+#define len(a) sizeof(a) / sizeof(int)
+#define what_is(x) cerr << #x << " is " << x << endl;
+#define all(x) (x).begin(), (x).end()
+
+void solve()
+{
+    int n, k; cin >> n >> k;
+    int a = n;
+    for (int i = 1; i <= n - (k+1); i++) 
+        cout << a-- << " ";
+    for (int i = 1; i <= k+1; i++)
+        cout << i << " ";
+    cout << endl;
+}
+int32_t main() {
+    Faster;
+    int t; cin >>t;   
+    while (t--) solve();
+    return 0;
+}
