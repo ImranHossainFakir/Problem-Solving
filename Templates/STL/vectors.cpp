7354ce8e486ca13a5a075385a08ee40f9bb4eca4
ImranHossain00 <imran188566@gmail.com>
2023-12-30 19:21:47 +0600

    updated

--- Templates/STL/vectors.cpp
+++ Templates/STL/vectors.cpp
@@ -1,1 +1,8 @@
+
 // Definin 2D vector
+// N = number of rows
+// {INF, INF} representing a vector of size 2, 
+// and we re initializing each row with this vector as the initial value.
+vector<vector<long long>> min_cost(N, {INF, INF});
+
+
