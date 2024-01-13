5be59556b41c93bc7ae785eb5d48b02de6f2b264
ImranHossain00 <imran188566@gmail.com>
2024-01-14 01:38:36 +0600

    Flight Discount

--- Regular/B_Glass_and_Mug.cpp
+++ Regular/B_Glass_and_Mug.cpp
@@ -2,28 +2,31 @@
 
 /*
 
-Problem link: 
-Time Complexity: 
+Problem link:
+Time Complexity:
 
 */
 
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
-//#define int long long
-#define endl "\n" 
-#define Faster ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define mem(a, b) memset(a, b ,sizeof(a))
+// #define int long long
+#define endl "\n"
+#define Faster                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define mem(a, b) memset(a, b, sizeof(a))
 #define len(a) sizeof(a) / sizeof(int)
 #define what_is(x) cerr << #x << " is " << x << endl;
 #define all(x) (x).begin(), (x).end()
 
 void solve()
 {
-    int k, g, m; cin >> k >> g >> m;
+    int k, g, m;
+    cin >> k >> g >> m;
     int mug = 0, glass = 0;
-    while (k--) 
+    while (k--)
     {
         if (glass == g)
             glass = 0;
@@ -31,21 +34,44 @@ void solve()
             mug = m;
         else if (glass)
         {
-            mug -= (g-glass);
+            mug -= (g - glass);
             glass = g;
-            
         }
-        else 
+        else
         {
             glass = min(g, mug);
             mug = max(mug - g, 0);
-        } 
+        }
     }
     cout << glass << " " << mug << endl;
 }
-int32_t main() {
-    Faster;
 
-    solve();
+void B()
+{
+    vector<int> arr(3);
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++) 
+        {
+
+            char c; cin >> c;
+            if (c == '?')
+                continue;
+            arr[c - 'A']++;
+        }
+    }
+    for (int i = 0; i < 3; i++)
+        if (arr[i] < 3)
+            cout << (char)(i + 'A') << endl;
+    
+}
+int32_t main()
+{
+    Faster;
+    int t;
+    cin >> t;
+    while (t--)
+        B();
+    // solve();
     return 0;
 }
--- Regular/Flight_Discount.cpp
+++ Regular/Flight_Discount.cpp
@@ -1,85 +1,86 @@
-#include <bits/stdc++.h>
+
+
+/*
+
+Problem link: https://cses.fi/problemset/task/1195/
+Time Complexity: O((n+m) log m) where n is nodes and m is edges.
+
+*/
+
+
+#include<bits/stdc++.h>
 
 using namespace std;
-// #define int long long
-#define endl "\n"
-#define Faster                    \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0);
-#define mem(a, b) memset(a, b, sizeof(a))
+#define int long long
+#define endl "\n" 
+#define Faster ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define mem(a, b) memset(a, b ,sizeof(a))
 #define len(a) sizeof(a) / sizeof(int)
 #define what_is(x) cerr << #x << " is " << x << endl;
 #define all(x) (x).begin(), (x).end()
 
-const int N = 1e5+9, INF = 1e9;
+const int N = 1e5 + 9, INF = -1;
 
-struct Node {
-    int at, cost;
-    Node(int _at, int _cost) {
-        at = _at;
-        cost = _cost;
-    }
+struct Node
+{       
+    int at, c;
 };
 
-struct CompareNode {
-    bool operator()(const Node& a, const Node& b) const {
+struct CompareNode
+{
+    bool operator()(const Node &a, const Node &b) const
+    {
         // Priority queue returns the smallest value.
         // So we need to write the comparator in a way
         // so that the smallest value becomes the greatest value.
-        return a.cost > b.cost;
+        return a.c > b.c;
     }
 };
 
-struct Edge {
-    int v, w;
-    Edge(int _v, int _w) {
-        v = _v;
-        w = _w;
-    }
-};
-
-vector < Edge > adj[N];
-priority_queue < Node, vector<Node>, CompareNode > pq;
-vector < int > dist;
-vector < bool > vis;
-vector < int > parents;
-vector < int > pathDist;
-
-void travers(int node)
+struct Edge
 {
-    // Difining the dist array 
-    // and setting the dist of start node as '0'.
-    dist.resize(N, INF);
+    int child, cost;
+};
+
+vector<Edge> normalAdj[N];
+vector<Edge> reverseAdj[N];
+vector<int> startDist;
+vector<int> endDist;
+
+// To find the shortest path from node 'node'.
+vector<int> dijkstra(int node, vector<Edge> adj[])
+{
+    vector<int> dist(N, INF);
+    vector<bool> vis(N, false);
+    priority_queue<Node, vector<Node>, CompareNode> pq;
+    // pushing start node as 0 distance node.
+    pq.push(Node{node, 0});
     dist[node] = 0;
-    pq.push(Node(node, 0));
-    vis.resize(N, false);
-    parents.resize(N, -1);
-    pathDist.resize(N);
 
     while (!pq.empty())
     {
-        // Picking the current lowest dist's node in tp.
-        Node tp = pq.top(); pq.pop();
-        vis[tp.at] = true;
+        // taking the current minimum distance node.
+        Node parent = pq.top();
+        pq.pop();
 
-        for (Edge e : adj[tp.at])
+        if (vis[parent.at])
+            continue;
+        // if a node is poped then it will not be evaluated/visited again.
+        // so marking as visited.
+        vis[parent.at] = true;
+
+        // traversing the adj/child nodes of the parent node.
+        // to update(if possible) the distances of the child nodes.
+        for (Edge e : adj[parent.at])
         {
-            if (vis[e.v])
-                continue;
-            // Checking the child nodes dist that
-            // is it neccessary to update the current dist that have the child node.
-            if (dist[e.v] > dist[tp.at] + e.w)
+            if (dist[e.child] == -1 || dist[e.child] > dist[parent.at] + e.cost)
             {
-                // updating the dist of child node
-                // and setting tp.at node as parent of the child node.
-                dist[e.v] = dist[tp.at] + e.w;
-                pq.push(Node(e.v, dist[e.v]));
-                parents[e.v] = tp.at;
-                pathDist[e.v] = e.w;
+                dist[e.child] = dist[parent.at] + e.cost;
+                pq.push(Node{e.child, dist[e.child]});
             }
         }
-    }   
+    }
+    return dist;
 }
 
 void solve()
@@ -90,28 +91,41 @@ void solve()
     // taking edges in the adj array
     for (int i = 0; i < m; i++)
     {
-        int x, y, w;
-        cin >> x >> y >> w;
-        adj[x].push_back(Edge(y, w));
+        int x, y, cost;
+        cin >> x >> y >> cost;
+        //Normal dicrection
+        normalAdj[x].push_back(Edge{y, cost});
+        //Reverse direction
+        reverseAdj[y].push_back(Edge{x, cost});
     }
 
-    // starting to traverse form start node 1
-    travers(1);
+    // Finding distance from node 1.
+    startDist = dijkstra(1, normalAdj);
+    // Finding distance from node n.
+    endDist = dijkstra(n, reverseAdj);
 
-    int sum = 0, mx = -1;
-    for (int cur = n; cur != -1; cur = parents[cur])
+
+    int minCost = INT64_MAX;
+    for (int city = 1; city <= n; city++) 
     {
-        sum += pathDist[cur];
-        mx = max(pathDist[cur], mx);
+        for (Edge e : normalAdj[city]) {
+            // if city or the adj cities are not reachable from
+            // nodes 1 or node n then we do not count the distance.
+            if (startDist[city] == INF || endDist[e.child] == INF)
+                continue;
+            // after using coupon finding minimum distance.
+            minCost = min(minCost, startDist[city] + e.cost / 2 + endDist[e.child]);
+
+        }
     }
 
-    cout << mx / 2 + (sum - mx) << endl;
+    cout << minCost << endl;
 }
 
 int32_t main()
 {
     Faster;
     solve();
-    
+
     return 0;
 }
