65cefcf2673085cce7688872a9cc0667afb95634
ImranHossain00 <imran188566@gmail.com>
2024-09-05 10:33:48 +0600

    I am back

---
+++ Templates/Complete_Search/findigPathsUsingBracktracking.cpp
@@ -0,0 +1,26 @@
+
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int cnt = 0, n;
+vector < vector < int > > grid(n, vector< int >(n));
+vector < int > flatGrid(n*n);
+vector < int > x{0, 0, 1, -1}, y{1, -1, 0, 0};
+
+void findPathsOfTheGrid(int x)
+{
+    if (x == n * n)
+    {
+        cnt++;
+        return;
+    }
+    // to be continued of implementation......
+}
+
+int32_t main() {
+    
+    return 0;
+}
